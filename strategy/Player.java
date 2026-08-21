29f8ea168d72b29aef65b60944e09a849471c427
teagan-donnelly <teaganad@email.sc.edu>
2026-08-21 19:06:45 -0400

    filled in attributes on UML diagram for Player

--- strategy/Player.java
+++ strategy/Player.java
@@ -1,5 +1,38 @@
 package strategy;
 
-public class Player implements Team {
+import java.util.ArrayList;
+import java.util.Random;
+
+public abstract class Player extends Team {
     
+    //initializes instance varibles
+    private String firstName;
+    private String lastName;
+    protected Behavior behavior;
+    protected Random rand;
+    protected PlayerType playerType; 
+
+    public Player(String teamName, ArrayList<Player> players, String firstName, String lastName, PlayerType playerType){
+        super(teamName, players); //calls parent class
+        //sets instance varibles
+        this.firstName = firstName;
+        this.lastName = lastName; 
+        this.behavior = behavior;
+        this.rand = rand;
+        this.playerType = playerType;
+    }
+
+    public abstract void setBehavior();
+
+    public String play(){
+        //TODO
+    }
+
+    public String toString(){
+        //TODO
+    }
+
+    public PlayerType getPlayerType(){
+        return playerType;
+    }
 }
