d524f0825b2d94c3690acf6c37c38280715fcb4d
teagan-donnelly <teaganad@email.sc.edu>
2026-09-05 13:51:04 -0400

    Added javadoc to Player

--- decorator/Player.java
+++ decorator/Player.java
@@ -8,15 +8,30 @@ public abstract class Player {
     protected String name;
     protected ArrayList<String> character = new ArrayList<>();
 
+    /**
+     * creates a new Player with the given parameters
+     * @param character the ArrayList containing all the Strings
+     * of the player
+     * @param name the name of the player
+     */
     public Player(ArrayList<String> character, String name){
         this.character = character;
         this.name = name;
     }
 
+    /**
+     * gets the name of the player
+     * @return the name of the player
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * loops through the character ArrayList and saves each line
+     * to a new string varible with line breaks in between
+     * @return the full string of the character
+     */
     public String toString(){
         String characterString = "";
 
