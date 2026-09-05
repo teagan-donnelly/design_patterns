29edcd25d9313b4e52d0ffa4899fc5fd1fcf1979
teagan-donnelly <teaganad@email.sc.edu>
2026-09-05 14:06:01 -0400

    Implemented Warrior and wrote java doc comments

--- decorator/Warrior.java
+++ decorator/Warrior.java
@@ -1,12 +1,16 @@
 package decorator;
 
 /**
- * 
+ * represents a warrior player
  * @author Teagan Donnelly
  */
 public class Warrior extends Player {
-
-    public void Warror(String name){
-        FileReader.getLines(warrior.txt);
+    /**
+     * creates a new warrior with the given name and loads the ASCII text file
+     * into an ArrayList
+     * @param name the name of the Player
+     */
+    public Warrior(String name){
+        super(FileReader.getLines("warrior.txt"), name);
     }
 }
