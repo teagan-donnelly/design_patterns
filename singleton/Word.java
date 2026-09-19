a9bc6d57ebf822fef566f26c081eeef35b4a7f2c
teagan-donnelly <teaganad@email.sc.edu>
2026-09-19 13:57:15 -0400

    Implemented Word.java

--- singleton/Word.java
+++ singleton/Word.java
@@ -17,10 +17,27 @@ public class Word {
     }
 
     public String getFlashCardFront(){
+        String reset = "\u001B[0m";
+        String cyan = "\u001B[36m";
 
+        String border = cyan + "+--------------------------------------------------------------+" + reset;
+        String blankLine = cyan + "|                                                              |" + reset;
+        String wordLine = cyan + "| " + reset + "Word: " + cyan + word.toUpperCase() + "                                                     |" + reset;
+
+        return border + wordLine + blankLine + blankLine + blankLine + border;
     }
 
     public String getFlashCardBack(){
+        String reset = "\u001B[0m";
+        String cyan = "\u001B[36m";
+        String pink = "\u001B[38;5;213m";
 
+        String border = cyan + "+--------------------------------------------------------------+" + reset;
+        String wordLine = cyan + "| " + reset + "Word: " + cyan + word.toUpperCase() + "                                                     |" + reset;
+        String typeLine = cyan + "| " + reset + "Part of Speech: " + cyan + type + "                                                     |" + reset;
+        String defLine = cyan + "| " + reset + "Definition: " + pink + definition + "                                                     |" + reset;
+        String sentLine = cyan + "| " + reset + "Sentance: " + pink + sentance + "                                                     |" + reset;
+
+        return border + wordLine + typeLine + defLine + sentLine;
     }
 }
