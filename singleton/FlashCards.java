9e9f35873647d4d66339d71ca7377cc7ec2642bf
teagan-donnelly <teaganad@email.sc.edu>
2026-09-19 12:48:24 -0400

    added outlined methods and variables

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -1,7 +1,25 @@
+import java.util.random.*;
+
 /**
  * 
  * @author Teagan Donnelly
  */
 public class FlashCards {
+
+    private Random rand;
+    private static FlashCards flashCards;
+    private ArrayList<Word> words;
+
+    private FlashCards(){
+
+    }
+
+    public static FlashCards getInstance(){
+
+    }
+
+    public Word getWord(){
+        
+    }
 
 }
--- singleton/Word.java
+++ singleton/Word.java
@@ -4,4 +4,20 @@
  */
 public class Word {
 
+    private String word;
+    private String type;
+    private String definition;
+    private String sentance;
+
+    public Word(String word, String type, Sting definition, String sentace){
+
+    }
+
+    public String getFlashCardFront(){
+
+    }
+
+    public String getFlashCardBack(){
+
+    }
 }
